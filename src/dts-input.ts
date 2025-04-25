3f0ce23214525242a707b55d6923e54631282f01
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-04-25 15:19:46 +0800

    fix: set `moduleSideEffects` for dts input
    closes #23

--- src/dts-input.ts
+++ src/dts-input.ts
@@ -5,6 +5,19 @@ export function createDtsInputPlugin(): Plugin {
   return {
     name: 'rolldown-plugin-dts:dts-input',
 
+    options(options) {
+      return {
+        treeshake:
+          options.treeshake !== false
+            ? {
+                ...(options.treeshake === true ? {} : options.treeshake),
+                moduleSideEffects: false,
+              }
+            : false,
+        ...options,
+      }
+    },
+
     outputOptions(options) {
       return {
         ...options,
