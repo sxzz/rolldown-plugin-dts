d5d039cbd1799f233402d6689f593c469f5c15c8
ocavue <ocavue@users.noreply.github.com>
2025-09-03 18:50:48 +1000

    test: add test for composite projects sourcemap (#97)
    Co-authored-by: Alex <alex@gogl.io>

---
+++ tests/fixtures/composite-refs-sourcemap/src/types.ts
@@ -0,0 +1,10 @@
+export type Toast = {
+  title: string
+  description?: string
+  duration?: number
+  type: "info" | "success" | "error" | "warning"
+  notificationId?: string
+}
+
+export const sharedValue = 1;
+
