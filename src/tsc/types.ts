87afc4a269101d4f959abb97fed650d2c7c542a2
ocavue <ocavue@users.noreply.github.com>
2025-09-04 19:54:30 +1000

    refactor: split tsc functionality into separate files (#98)

---
+++ src/tsc/types.ts
@@ -0,0 +1,28 @@
+import type { TscContext } from './context.ts'
+import type { TsConfigJson } from 'get-tsconfig'
+import type { SourceMapInput } from 'rolldown'
+import type ts from 'typescript'
+
+export interface TscModule {
+  program: ts.Program
+  file: ts.SourceFile
+}
+
+export interface TscOptions {
+  tsconfig?: string
+  tsconfigRaw: TsConfigJson
+  cwd: string
+  build: boolean
+  incremental: boolean
+  entries?: string[]
+  id: string
+  sourcemap: boolean
+  vue?: boolean
+  context?: TscContext
+}
+
+export interface TscResult {
+  code?: string
+  map?: SourceMapInput
+  error?: string
+}
