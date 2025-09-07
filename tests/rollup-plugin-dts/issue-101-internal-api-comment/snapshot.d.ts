9d2be6b30d7a1c79f1e1b7fe58e294461b6fa829
ocavue <ocavue@gmail.com>
2025-09-07 15:30:24 +1000

    test: add test case for issue 101

---
+++ tests/rollup-plugin-dts/issue-101-internal-api-comment/snapshot.d.ts
@@ -0,0 +1,20 @@
+// index.d.ts
+//#region tests/rollup-plugin-dts/issue-101-internal-api-comment/api.d.ts
+/**
+ * JSDoc for public_api_with_jsdoc
+ */
+declare function public_api_with_jsdoc(): void;
+
+/**
+ * JSDoc for internal_api_with_jsdoc
+ * 
+ * This comment should NOT occur in dist/index.d.ts
+ */
+
+declare function public_api_without_jsdoc(): void;
+//#endregion
+//#region tests/rollup-plugin-dts/issue-101-internal-api-comment/index.d.ts
+/** JSDoc for public_type */
+type public_type = number;
+//#endregion
+export { public_api_with_jsdoc, public_api_without_jsdoc, public_type };
