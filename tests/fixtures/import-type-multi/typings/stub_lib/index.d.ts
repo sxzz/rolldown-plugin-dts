a9a589e7448aecdc4e4acbb80f221a83192be4c8
Jungzl <13jungzl@gmail.com>
2026-02-02 11:23:45 +0800

    fix: ensure deterministic and valid namespace import identifiers across files

---
+++ tests/fixtures/import-type-multi/typings/stub_lib/index.d.ts
@@ -0,0 +1,3 @@
+export interface LibType {
+  name: string;
+}
--- tests/rollup-plugin-dts/inline-import-external-namespace/index.d.ts
+++ tests/rollup-plugin-dts/inline-import-external-namespace/index.d.ts
@@ -1,4 +1,8 @@
 export interface Foo {
   ns1: import("foo");
   ns2: typeof import("foo");
+  ns3: import('foo_bar').T;
+  ns4: import('foo-bar').T;
+  ns5: import('foo.bar').T;
+  ns6: import('foo/bar').T;
 }
