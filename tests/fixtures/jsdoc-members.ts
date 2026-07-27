7d1f56e20ecc536769769a820e82ca673bbe83bf
Ben Snyder <ben@snyder.tech>
2026-07-27 01:04:32 -0400

    test: JSDoc comment positioning in type literal members (#217)
    Co-authored-by: Kevin Deng <sxzz@sxzz.moe>

---
+++ tests/fixtures/jsdoc-members.ts
@@ -0,0 +1,21 @@
+export const foo: string = 'foo'
+
+export interface A {
+  /** Comment A1 */
+  AA?: string
+  /** Comment A2 */
+  AB?: string
+  /** Comment A3 */
+  AC?: string
+}
+
+export type B = {
+  /** Comment B1 */
+  BA?: string
+  /** Comment B2 */
+  BB?: string
+  /** Comment B3 */
+  BC?: string
+}
+
+export function fn(): void {}
